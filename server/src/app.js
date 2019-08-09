682e479eef2766bb98557cd1b184b3edaf19909e
James Jiang <jamesjiang282@gmail.com>
2019-08-08 20:07:27 -0700

    Added a package json along with a server file

---
+++ server/src/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyparser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
+const normalizePort = require('normalize-port');
+const app = express();
+
+const port = normalizePort(process.env.Port || '8801');
+
+app.get('/', function(req, res) {
+  res.send('Hello World!');
+})
+
