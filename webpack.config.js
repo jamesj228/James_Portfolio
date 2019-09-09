3369779c3a083a0b28092accfcadbaa26c4009ef
jamesj228 <jamesjiang282@gmail.com>
2019-09-08 22:51:17 -0700

    Added babelrc and webpack

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+var path = require('path');
+var SRC_DIR = require('/client/src');
+var DIST_DIR = require('/client/dist');
+var uglifyPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+	module: {
+		rules: [
+			{
+				test:
+			}
+		]
+	}
+}
